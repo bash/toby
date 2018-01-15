b587db6bf41d4a54ee66b72251319b66f32bd259
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2018-01-15 23:44:29 +0100

    Fix package script

--- package/package.sh
+++ package/package.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 tar -zcvf toby.tar.gz ./conf ./units ./src Cargo.toml Cargo.lock
+
+mkdir -p $HOME/rpmbuild/SOURCES
 cp toby.tar.gz $HOME/rpmbuild/SOURCES
+
 rpmbuild -bb package/package.spec
