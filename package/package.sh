cfbdfefde245960faff2b9824226f86e672f9c75
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2018-01-20 15:22:24 +0100

    Try to build with fixed openssl version

--- package/package.sh
+++ package/package.sh
@@ -3,6 +3,6 @@
 tar -zcvf toby.tar.gz ./conf ./units ./src Cargo.toml Cargo.lock
 
 mkdir -p $HOME/rpmbuild/SOURCES
-cp toby.tar.gz $HOME/rpmbuild/SOURCES
+mv toby.tar.gz $HOME/rpmbuild/SOURCES
 
 rpmbuild -bb package/package.spec
