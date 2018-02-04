1e4f4bc84207db48eb749cf0d101f11e74500cfb
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2018-02-04 12:33:52 +0100

    Enable crate_in_path feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-#![feature(plugin, decl_macro, option_filter, slice_concat_ext, custom_derive, use_extern_macros)]
+#![feature(plugin, decl_macro, option_filter, slice_concat_ext, custom_derive, use_extern_macros,
+           crate_in_paths)]
 #![plugin(rocket_codegen)]
 
 extern crate byteorder;
