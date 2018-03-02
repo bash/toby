304b80efc9607e76b3e3520062eae301c6b53321
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2018-03-02 17:58:23 +0100

    Revert "Build static binaries with musl"
    This reverts commit 0484a4e83e4d9611fb0295fef587607980486aba.

--- .travis/install.sh
+++ .travis/install.sh
@@ -15,10 +15,17 @@ then
   curl -O https://www.openssl.org/source/openssl-$OPENSSL_VERSION.tar.gz
   tar -zxf openssl-$OPENSSL_VERSION.tar.gz
   cd openssl-$OPENSSL_VERSION
-  CC=musl-gcc ./Configure no-dso no-ssl2 no-ssl3 linux-x86_64 -fPIC --prefix="$OPENSSL_DIR"
+  ./config shared no-asm no-ssl2 no-ssl3 -fPIC --prefix="$OPENSSL_DIR"
 
-  make -j$(nproc)
-  make install
+  # modify the shlib version to a unique one to make sure the dynamic
+  # linker doesn't load the system one. This isn't required for 1.1.0 at the
+  # moment since our Travis builders have a diff shlib version, but it doesn't hurt
+  # sed -i "s/^SHLIB_MAJOR=.*/SHLIB_MAJOR=100/" Makefile
+  # sed -i "s/^SHLIB_MINOR=.*/SHLIB_MINOR=0.0/" Makefile
+  # sed -i "s/^SHLIB_VERSION_NUMBER=.*/SHLIB_VERSION_NUMBER=100.0.0/" Makefile
+
+  make --quiet depend
+  make --quiet install
 fi
 
 echo -en "travis_fold:end:openssl-install\\r"
--- .travis/prepare-deploy.sh
+++ .travis/prepare-deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# if [ -n "$OPENSSL_VERSION" ]
-# then
-#   mv $HOME/rpmbuild/RPMS/x86_64/toby-$TRAVIS_TAG-1.x86_64.rpm \
-#      $HOME/rpmbuild/RPMS/x86_64/toby-$TRAVIS_TAG-1.x86_64.openssl.$OPENSSL_VERSION.rpm
-# fi
+if [ -n "$OPENSSL_VERSION" ]
+then
+  mv $HOME/rpmbuild/RPMS/x86_64/toby-$TRAVIS_TAG-1.x86_64.rpm \
+     $HOME/rpmbuild/RPMS/x86_64/toby-$TRAVIS_TAG-1.x86_64.openssl.$OPENSSL_VERSION.rpm
+fi
