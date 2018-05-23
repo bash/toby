d925038c8e99546ff3e79292b46324662bcfbabf
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2018-05-23 14:41:56 +0200

    Improve install script
    - Rename environment variable `TARGET` to `TOBY_TARGET`
    - Allow using a prefix for the installation location

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,7 +2,17 @@
 
 set -e
 
-cp ./target/$TARGET/toby ./target/$TARGET/tobyd /usr/local/bin/
+if [ -z "$TOBY_TARGET" ]
+then
+  echo "Environment variable TOBY_TARGET is not set."
+  echo ""
+  echo "Example usage:"
+  echo "TOBY_TARGET=release $0 $@"
+  exit 1
+fi
 
-mkdir -p /usr/local/etc/toby/conf.d
-cp ./conf/*.toml /usr/local/etc/toby/
+mkdir -p $TOBY_PREFIX/usr/local/bin/
+cp ./target/$TOBY_TARGET/toby ./target/$TOBY_TARGET/tobyd $TOBY_PREFIX/usr/local/bin/
+
+mkdir -p $TOBY_PREFIX/usr/local/etc/toby/conf.d
+cp ./conf/*.toml $TOBY_PREFIX/usr/local/etc/toby/
