2b7dabc8c26703e29f8d89c87e0d2f9beb5326ca
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2018-08-05 14:29:30 +0200

    Run cargo fmt

--- toby-core/src/model/job.rs
+++ toby-core/src/model/job.rs
@@ -4,64 +4,64 @@ pub type JobId = u64;
 
 #[derive(Debug)]
 pub struct Job {
-  pub id: JobId,
-  pub project: String,
-  pub trigger: JobTrigger,
+    pub id: JobId,
+    pub project: String,
+    pub trigger: JobTrigger,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ArchivedJob {
-  pub started_at: u64,
-  pub successful: bool,
-  pub trigger: ArchivedJobTrigger,
+    pub started_at: u64,
+    pub successful: bool,
+    pub trigger: ArchivedJobTrigger,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone, Eq, PartialEq)]
 pub enum JobTrigger {
-  Webhook { token: String },
-  Cli,
+    Webhook { token: String },
+    Cli,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 #[serde(tag = "type", rename_all = "snake_case")]
 pub enum ArchivedJobTrigger {
-  Webhook { token: String },
-  Cli,
+    Webhook { token: String },
+    Cli,
 }
 
 impl JobTrigger {
-  pub fn name(&self) -> &str {
-    match *self {
-      JobTrigger::Webhook { .. } => "webhook",
-      JobTrigger::Cli => "cli",
+    pub fn name(&self) -> &str {
+        match *self {
+            JobTrigger::Webhook { .. } => "webhook",
+            JobTrigger::Cli => "cli",
+        }
     }
-  }
 }
 
 impl From<JobTrigger> for ArchivedJobTrigger {
-  fn from(trigger: JobTrigger) -> Self {
-    match trigger {
-      JobTrigger::Cli => ArchivedJobTrigger::Cli,
-      JobTrigger::Webhook { token } => ArchivedJobTrigger::Webhook { token },
+    fn from(trigger: JobTrigger) -> Self {
+        match trigger {
+            JobTrigger::Cli => ArchivedJobTrigger::Cli,
+            JobTrigger::Webhook { token } => ArchivedJobTrigger::Webhook { token },
+        }
     }
-  }
 }
 
 impl fmt::Display for JobTrigger {
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    match *self {
-      JobTrigger::Webhook { ref token } => write!(f, "webhook ({})", token),
-      JobTrigger::Cli => write!(f, "cli"),
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            JobTrigger::Webhook { ref token } => write!(f, "webhook ({})", token),
+            JobTrigger::Cli => write!(f, "cli"),
+        }
     }
-  }
 }
 
 impl Job {
-  pub fn archive(&self, started_at: u64, successful: bool) -> ArchivedJob {
-    ArchivedJob {
-      trigger: self.trigger.clone().into(),
-      started_at,
-      successful,
+    pub fn archive(&self, started_at: u64, successful: bool) -> ArchivedJob {
+        ArchivedJob {
+            trigger: self.trigger.clone().into(),
+            started_at,
+            successful,
+        }
     }
-  }
 }
--- toby-core/tests/ipc.rs
+++ toby-core/tests/ipc.rs
@@ -7,39 +7,39 @@ use toby_core::model::job::JobTrigger;
 
 #[test]
 fn test_ipc() {
-  let (tx, rx) = channel();
-  let (ready_tx, ready_rx) = channel();
+    let (tx, rx) = channel();
+    let (ready_tx, ready_rx) = channel();
 
-  thread::spawn(move || {
-    let mut server = IpcServer::bind().unwrap();
+    thread::spawn(move || {
+        let mut server = IpcServer::bind().unwrap();
 
-    ready_tx.send(()).unwrap();
+        ready_tx.send(()).unwrap();
 
-    let message = server.receive().unwrap();
+        let message = server.receive().unwrap();
 
-    tx.send(message).unwrap();
-  });
+        tx.send(message).unwrap();
+    });
 
-  thread::spawn(move || {
-    ready_rx.recv().unwrap();
+    thread::spawn(move || {
+        ready_rx.recv().unwrap();
 
-    let mut client = IpcClient::connect().unwrap();
+        let mut client = IpcClient::connect().unwrap();
 
-    client
-      .send(&IpcMessage::Job {
-        trigger: JobTrigger::Cli,
-        project: "foo".into(),
-      })
-      .unwrap();
-  });
+        client
+            .send(&IpcMessage::Job {
+                trigger: JobTrigger::Cli,
+                project: "foo".into(),
+            })
+            .unwrap();
+    });
 
-  let message = rx.recv().unwrap();
+    let message = rx.recv().unwrap();
 
-  assert_eq!(
-    IpcMessage::Job {
-      trigger: JobTrigger::Cli,
-      project: "foo".into(),
-    },
-    message
-  );
+    assert_eq!(
+        IpcMessage::Job {
+            trigger: JobTrigger::Cli,
+            project: "foo".into(),
+        },
+        message
+    );
 }
