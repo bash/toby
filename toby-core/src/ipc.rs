d579651d0bac0907384228271479e2c687b76a7b
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2018-08-05 11:37:32 +0200

    Re-order items in ipc mod

--- toby-core/src/ipc.rs
+++ toby-core/src/ipc.rs
@@ -11,118 +11,118 @@ use std::path::PathBuf;
 
 #[derive(Debug)]
 pub enum Error {
-  Io(io::Error),
-  Bincode(bincode::Error),
-}
-
-impl fmt::Display for Error {
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    match self {
-      Error::Io(ref err) => write!(f, "{}", err),
-      Error::Bincode(ref err) => write!(f, "{}", err),
-    }
-  }
-}
-
-impl error::Error for Error {
-  fn cause(&self) -> Option<&dyn error::Error> {
-    match self {
-      Error::Io(ref err) => Some(err),
-      Error::Bincode(ref err) => Some(err),
-    }
-  }
-}
-
-impl From<io::Error> for Error {
-  fn from(err: io::Error) -> Self {
-    Error::Io(err)
-  }
-}
-
-impl From<bincode::Error> for Error {
-  fn from(err: bincode::Error) -> Self {
-    Error::Bincode(err)
-  }
-}
-
-fn socket_path() -> io::Result<PathBuf> {
-  let mut path = PathBuf::from(RUNTIME_PATH);
-
-  if !path.exists() {
-    create_dir(&path)?;
-  }
-
-  path.push("toby.sock");
-
-  Ok(path)
+    Io(io::Error),
+    Bincode(bincode::Error),
 }
 
 #[derive(Debug)]
 pub struct IpcServer<T>
 where
-  T: for<'de> Deserialize<'de>,
+    T: for<'de> Deserialize<'de>,
 {
-  inner: UnixListener,
-  phantom: PhantomData<T>,
+    inner: UnixListener,
+    phantom: PhantomData<T>,
 }
 
 #[derive(Debug)]
 pub struct IpcClient<T>
 where
-  T: Serialize,
+    T: Serialize,
 {
-  inner: UnixStream,
-  phantom: PhantomData<T>,
+    inner: UnixStream,
+    phantom: PhantomData<T>,
+}
+
+fn socket_path() -> io::Result<PathBuf> {
+    let mut path = PathBuf::from(RUNTIME_PATH);
+
+    if !path.exists() {
+        create_dir(&path)?;
+    }
+
+    path.push("toby.sock");
+
+    Ok(path)
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Error::Io(ref err) => write!(f, "{}", err),
+            Error::Bincode(ref err) => write!(f, "{}", err),
+        }
+    }
+}
+
+impl error::Error for Error {
+    fn cause(&self) -> Option<&dyn error::Error> {
+        match self {
+            Error::Io(ref err) => Some(err),
+            Error::Bincode(ref err) => Some(err),
+        }
+    }
+}
+
+impl From<io::Error> for Error {
+    fn from(err: io::Error) -> Self {
+        Error::Io(err)
+    }
+}
+
+impl From<bincode::Error> for Error {
+    fn from(err: bincode::Error) -> Self {
+        Error::Bincode(err)
+    }
 }
 
 impl<T> IpcServer<T>
 where
-  T: for<'de> Deserialize<'de>,
+    T: for<'de> Deserialize<'de>,
 {
-  pub fn bind() -> io::Result<Self> {
-    let path = socket_path()?;
+    pub fn bind() -> io::Result<Self> {
+        let path = socket_path()?;
 
-    if path.exists() {
-      remove_file(&path)?;
+        if path.exists() {
+            remove_file(&path)?;
+        }
+
+        Ok(Self {
+            inner: UnixListener::bind(path)?,
+            phantom: PhantomData,
+        })
     }
 
-    Ok(Self {
-      inner: UnixListener::bind(path)?,
-      phantom: PhantomData,
-    })
-  }
+    pub fn receive(&mut self) -> Result<T, Error> {
+        let (socket, _) = self.inner.accept()?;
+        let mut reader = BufReader::new(socket);
 
-  pub fn receive(&mut self) -> Result<T, Error> {
-    let (socket, _) = self.inner.accept()?;
-    let mut reader = BufReader::new(socket);
+        let mut buf = Vec::new();
 
-    let mut buf = Vec::new();
+        reader.read_to_end(&mut buf)?;
 
-    reader.read_to_end(&mut buf)?;
-
-    Ok(deserialize(&buf)?)
-  }
+        Ok(deserialize(&buf)?)
+    }
 }
 
 impl<T> IpcClient<T>
 where
-  T: Serialize,
+    T: Serialize,
 {
-  pub fn connect() -> io::Result<Self> {
-    let path = socket_path()?;
+    pub fn connect() -> io::Result<Self> {
+        let path = socket_path()?;
 
-    Ok(Self {
-      inner: UnixStream::connect(path)?,
-      phantom: PhantomData,
-    })
-  }
+        Ok(Self {
+            inner: UnixStream::connect(path)?,
+            phantom: PhantomData,
+        })
+    }
 
-  pub fn send(&mut self, value: &T) -> Result<(), Error> {
-    let mut writer = BufWriter::new(&mut self.inner);
-    let encoded = serialize(value)?;
+    pub fn send(&mut self, value: &T) -> Result<(), Error> {
+        let mut writer = BufWriter::new(&mut self.inner);
+        let encoded = serialize(value)?;
 
-    writer.write(&encoded)?;
+        writer.write(&encoded)?;
 
-    Ok(())
-  }
+        Ok(())
+    }
 }
