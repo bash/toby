d7452676ec42f87ac9daab1a4939f5c2e0d7526f
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2018-08-07 22:44:46 +0200

    Add integration test for config loading

---
+++ toby-core/tests/config.rs
@@ -0,0 +1,52 @@
+#![feature(decl_macro)]
+
+use self::context::TempContext;
+use std::fs::{create_dir, OpenOptions};
+use std::io::Write;
+use toby_core::{config::Config, path};
+
+mod context;
+
+#[test]
+fn test_load_config_works() {
+    let context = TempContext::create().unwrap();
+    let config_path = context.config_path();
+
+    macro write_file($path:expr, $contents:expr) {
+        let mut file = OpenOptions::new()
+            .create(true)
+            .write(true)
+            .open($path)
+            .expect("unable to open file");
+
+        file.write_all($contents).expect("unable to write file");
+    };
+
+    write_file!(
+        path!(config_path, "toby.toml"),
+        br#"[listen]
+address = "172.16.16.16"
+port = 1234"#
+    );
+
+    write_file!(
+        path!(config_path, "tokens.toml"),
+        br#"[robot]
+secret = "much_secret_wow"
+access = ["dreams"]"#
+    );
+
+    let projects_path = path!(config_path, "projects.d");
+    create_dir(&projects_path).expect("unable to create dir");
+
+    write_file!(
+        path!(&projects_path, "dreams.toml"),
+        br#"[[scripts]]
+command = ["systemctl", "restart", "dreams"]
+allow_failure = true"#
+    );
+
+    let config = Config::load(&context);
+
+    assert!(config.is_ok());
+}
