47250020157630f64a4688b0ad34467dfc143641
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2018-08-26 12:21:07 +0200

    Connect with socket in toby-httpd

--- toby-cli/src/main.rs
+++ toby-cli/src/main.rs
@@ -1,16 +1,3 @@
 #![warn(rust_2018_idioms)]
 
-use toby_core::ipc::{IpcClient, IpcMessage};
-use toby_core::job::JobTrigger;
-use toby_core::Context;
-
-fn main() {
-    let context = Context::default_context();
-    let mut client = IpcClient::connect(context).unwrap();
-
-    client
-        .send(&IpcMessage::Job {
-            project: "foo".into(),
-            trigger: JobTrigger::Cli,
-        }).unwrap();
-}
+fn main() {}
--- toby-httpd/src/main.rs
+++ toby-httpd/src/main.rs
@@ -1,5 +1,23 @@
 #![warn(rust_2018_idioms)]
 
+use toby_core::config::{ConfigLoader, FsConfigSource};
+#[cfg(feature = "enable-user-switch")]
+use toby_core::identity::{set_current, Identity};
+use toby_core::ipc::IpcClient;
+use toby_core::Context;
+
 fn main() {
-    println!("Hello, world!");
+    let context = Context::default_context();
+
+    let config = ConfigLoader::new(&FsConfigSource::new(context))
+        .load()
+        .unwrap();
+
+    #[cfg(feature = "enable-user-switch")]
+    {
+        let identity = Identity::load(config.user(), config.group()).unwrap();
+        set_current(&identity).unwrap();
+    }
+
+    let mut client = IpcClient::connect(context).unwrap();
 }
