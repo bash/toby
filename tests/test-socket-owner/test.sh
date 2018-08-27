9ac43090e998ffab5b827d0c2f25f016257589fd
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2018-08-27 20:21:27 +0200

    Revert "Use `sudo` to run docker"
    This reverts commit e38a1bfce0177e8fbccb799b6a9ff21f2a8e213a.

--- tests/test-socket-owner/test.sh
+++ tests/test-socket-owner/test.sh
@@ -4,13 +4,13 @@ TEST_DIR=$(cd $(dirname "$0") && pwd)
 
 source "$TEST_DIR/../_assert.sh"
 
-sudo docker build -t toby-test-socket-owner -f "$TEST_DIR/Dockerfile" .
+docker build -t toby-test-socket-owner -f "$TEST_DIR/Dockerfile" .
 
 CONTAINER_ID=$(docker run -d toby-test-socket-owner)
 
-OWNER=$(sudo docker exec "$CONTAINER_ID" stat /usr/local/var/lib/toby/toby-workerd.sock -c '%U:%G')
+OWNER=$(docker exec "$CONTAINER_ID" stat /usr/local/var/lib/toby/toby-workerd.sock -c '%U:%G')
 
-sudo docker stop "$CONTAINER_ID" > /dev/null
-sudo docker rm "$CONTAINER_ID" > /dev/null
+docker stop "$CONTAINER_ID" > /dev/null
+docker rm "$CONTAINER_ID" > /dev/null
 
 assert_eq "toby:toby" "$OWNER"
