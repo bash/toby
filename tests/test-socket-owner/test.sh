e38a1bfce0177e8fbccb799b6a9ff21f2a8e213a
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2018-08-27 20:14:28 +0200

    Use `sudo` to run docker

--- tests/test-socket-owner/test.sh
+++ tests/test-socket-owner/test.sh
@@ -4,13 +4,13 @@ TEST_DIR=$(cd $(dirname "$0") && pwd)
 
 source "$TEST_DIR/../_assert.sh"
 
-docker build -t toby-test-socket-owner -f "$TEST_DIR/Dockerfile" .
+sudo docker build -t toby-test-socket-owner -f "$TEST_DIR/Dockerfile" .
 
 CONTAINER_ID=$(docker run -d toby-test-socket-owner)
 
-OWNER=$(docker exec "$CONTAINER_ID" stat /usr/local/var/lib/toby/toby-workerd.sock -c '%U:%G')
+OWNER=$(sudo docker exec "$CONTAINER_ID" stat /usr/local/var/lib/toby/toby-workerd.sock -c '%U:%G')
 
-docker stop "$CONTAINER_ID" > /dev/null
-docker rm "$CONTAINER_ID" > /dev/null
+sudo docker stop "$CONTAINER_ID" > /dev/null
+sudo docker rm "$CONTAINER_ID" > /dev/null
 
 assert_eq "toby:toby" "$OWNER"
